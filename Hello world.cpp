fde1fc3e28a4e93f16618f375cf2b14a38a23c46
RIKM563 <71692739+RIKM563@users.noreply.github.com>
2021-03-17 11:23:08 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
  
 int main(int argc, char** argv){
- string name; \\ hehehey
- std::cin >> name; \\ ququququ
+ string name; \\ hehehey.
+ std::cin >> name; \\ кукукуку
  std::cout << "Hello world from " << name << std::endl;
 } 
