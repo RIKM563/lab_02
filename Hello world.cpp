5e25571cbcf5e5a4462854f1c4434d295ec59b56
RIKM563 <artemshabalin563@mail.ru>
2021-03-17 11:29:19 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -4,15 +4,15 @@
 
 int main(int argc, char** argv)
 {
-  string name; \\ hehehey.
+  string name;\\ hehehey.
   std::cin >> name;\\ кукукуку
   std::cout << "Hello world from " << name << std::endl;
 }
 =======
  
 int main(int argc, char** argv){
- string name; \\ hehehey.
- std::cin >> name; \\ кукукуку
+ string name;\\ hehehey.
+ std::cin >> name;\\ кукукуку
  std::cout << "Hello world from " << name << std::endl;
 } 
 >>>>>>> fde1fc3e28a4e93f16618f375cf2b14a38a23c46
